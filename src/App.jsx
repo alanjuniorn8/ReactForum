e3f93c6f1a9b04e89d344b460371185dbdf4047e
Alan Spínola <alanjuniornunu8@gmail.com>
2023-03-08 11:07:32 -0300

    Add behaviors

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,39 @@ import { Post } from './components/Post';
 import styles from './App.module.css'
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/alanjuniorn8.png',
+      name:'Alan Spínola',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-03-07 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/alanjuniorn8.png',
+      name:'Alan Spínola',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-03-05 20:00:00')
+  },
+]
+
+
+
 export function App() {
   return (
     <>
@@ -12,8 +45,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,43 +1,114 @@
 import { Avatar } from './Avatar';
 import { Comment } from './Comment'
-import styles from './Post.module.css';
 
-export function Post() {
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './Post.module.css';
+import { useState } from 'react';
+
+export function Post({author, publishedAt, content}) {
+
+  const [comments, setComments]= useState(['LEGAL!!!']);
+  const [newCommentText, setNewCommentText]= useState('');
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    if(newCommentText) setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentText() {
+    event.target.setCustomValidity('');
+    setNewCommentText(event.target.value);
+  }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment != commentToDelete;
+    });
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Escreva algo antes!');
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
+
+  // const publishedDateFormated = new Intl.DateTimeFormat('pt-BR', {
+  //   day: '2-digit',
+  //   month: 'long',
+  //   hour: '2-digit',
+  //   minute:'2-digit'
+  // }).format(publishedAt);
+
+  const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
+    locale: ptBR
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
+
+  const publishedDateISOString = publishedAt.toISOString();
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/alanjuniorn8.png"/>
+          <Avatar src={author.avatarUrl}/>
           <div className={styles.authorInfo}>
-            <strong>Alan Spínola</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-      <time title='07 de março às 13h30m' dateTime='2023-07-03 13:30:00'>
-        Publicado há 1h
+        <time title={publishedDateFormated} dateTime={publishedDateISOString}>
+        {publishedDateRelativeToNow}
       </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉{' '}<a href='#'>jane.design/doctorcare</a></p>
-        <p><a href='#'>#novoprojeto</a>{' '}<a href='#'>#nlw</a>{' '}<a href='#'>#rocketseat</a></p> 
+        {content.map(line => {
+          if(line.type == 'paragraph') {
+            return (<p key={line.content}>{line.content}</p>);
+          } else if (line.type == 'link') {
+            return (<p key={line.content}><a href='#'>{line.content}</a></p>);
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea placeholder='Deixe um comentário'/>
+        <textarea 
+          name='comment' 
+          placeholder='Deixe um comentário'
+          value={newCommentText}
+          onChange={handleNewCommentText}
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return(
+            <Comment 
+              key={comment} 
+              content={comment} 
+              onDeleteComment={deleteComment}
+            />
+          );
+        })}
       </div>
     </article>
   )
